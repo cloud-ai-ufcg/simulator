5d774b09ec1c1db3cffe7c0bdb0e1c2dd3057fb0
JWillianSl <jose.williansl@hotmail.com>
2026-02-08 23:34:58 -0300

    feat: modify the setup-environment script to delete previous karmada config files

--- initializer/setup-environment.sh
+++ initializer/setup-environment.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -e 
 
 KUBE_PATH="$HOME/.kube"
+
+# Clean up existing Karmada and members config files if they are empty
+sudo rm -f "$KUBE_PATH/karmada.config" "$KUBE_PATH/members.config"
 
 # Setup the infrastructure environment
 docker compose up -d infra-environment
 
+# Wait until the Karmada and members config files are generated
 echo "Waiting for Karmada config to be generated..."
 while [ ! -s "$KUBE_PATH/karmada.config" ] || [ ! -s "$KUBE_PATH/members.config" ]; do
     sleep 10
